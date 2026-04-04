c805653bf0d236f368b6264262127855472b176e
ClaytonK007 <72389807+ClaytonK007@users.noreply.github.com>
2026-04-04 21:30:22 +0200

    Update 2.ToDoList.py

--- Python & SQL database projects/ToDo List/2.ToDoList.py
+++ Python & SQL database projects/ToDo List/2.ToDoList.py
@@ -5,8 +5,8 @@ from colorama import Fore, Style
 # create databse connection
 mydb = mysql.connector.connect(
     host = "localhost",
-    user = "Admin",
-    password = "SQLpassword!",
+    user = "(your username)",
+    password = "(your password)",
     database = "tasks"
     )
 
