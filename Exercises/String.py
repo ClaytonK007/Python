68a96226d60c7414a447dbea3987957bf2251d2f
ClaytonK007 <72389807+ClaytonK007@users.noreply.github.com>
2026-04-28 21:40:29 +0200

    Add files via upload
    updated string

--- Exercises/String.py
+++ Exercises/String.py
@@ -126,3 +126,124 @@ if str1.startswith("https") and str1.endswith(".com"):
     print("Is valid URL: True")
 else:
     print("Is valid URL: False")
+
+#
+#   11. Swap Case
+#
+str1 = "PyThOn"
+swap = str1.swapcase()
+
+print(swap)
+
+#
+#   12. Remove Whitespace
+#
+str1 = " P y t h o n "
+modified = str1.replace(" ", "")
+
+print(modified)
+
+#
+#   13. N-th Character Removal
+#
+str1 = "Python"
+i = 2
+
+first = str1[:i]
+second = str1[i + 1:]
+
+result = first + second
+print("After removing index", i, ":", result)
+
+#
+#   14. String Partitioning
+#
+str1 = "username@company.com"
+
+modified = str1.partition("@")
+
+print("Original string:", str1)
+print("Partitioned result:", modified)
+print("Username:", modified[0])
+
+#
+#   15. Extract File Extension
+#
+file_name = "report_final_v2.pdf"
+modified = file_name.split(".")[-1]
+
+print("File name:", file_name)
+print("Extension:", modified)
+
+#
+#   16. Lowercase First
+#
+str1 = "PyNaTive"
+lower = []
+upper = []
+
+for char in str1:
+      if char.islower():
+            lower.append(char)
+      else:
+            upper.append(char)
+
+result = "".join(lower + upper)
+print("Result:", result)
+
+#
+#   17. Count all letters, digits, and special symbols from a given string
+#
+def count(string):
+      char_count = 0
+      digit_count = 0
+      symbol_count = 0
+
+      for char in string:
+            if char.isalpha():
+                  char_count += 1
+            elif char.isdigit():
+                  digit_count += 1
+            else:
+                  symbol_count += 1
+
+      print(f"Total count, Characters: {char_count}, Digits: {digit_count}, Symbols: {symbol_count}")
+
+count("P@#yn26at^&i5ve")
+
+#
+#   18. Create a mixed string using alternating characters
+#
+s1 = "Abc" 
+s2 = "Xyz"
+
+s1_len = len(s1)
+s2_len = len(s2)
+
+length = s1_len if s1_len > s2_len else s2_len
+result = ""
+
+s2 = s2[::-1]
+
+for i in range(length):
+      if i < s1_len:
+            result = result + s1[i]
+      if i < s2_len:
+            result = result + s2[i]
+
+print(result)
+
+#
+#   19. Calculate the sum and average of the digits present in a string
+#
+str1 = "PYnative29@#8496"
+total_sum = 0
+count = 0
+
+for char in str1:
+    if char.isdigit():
+        total_sum += int(char)
+        count += 1
+
+average = total_sum / count
+print("Sum is:", {total_sum} ,"Average:", {average})
