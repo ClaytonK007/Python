0d20e138bfac9361d7889f5a691c31af04d1c6fe
ClaytonK007 <72389807+ClaytonK007@users.noreply.github.com>
2026-05-04 15:09:04 +0200

    Add files via upload
    added word guess

---
+++ Simple Projects/WordGuess.py
@@ -0,0 +1,39 @@
+import random
+
+name = input("What is your name? ")
+print("Hi there", name, ". Good Luck!")
+
+words = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks']
+word = random.choice(words)
+
+print("Guess the characters in the word:")
+guesses = ""
+turns = 12
+
+while turns > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_")
+            failed += 1
+    if failed == 0:
+        print("Congrats. You win!")
+        print("The word is:", word)
+        break
+    
+    print()
+    guess = input("Guess a character:")
+
+    guesses += guess
+
+    if guess not in word:
+        turns -= 1
+        print("Wrong. Try again.")
+        print("You have", + turns, "more guesses left.")
+
+        if turns == 0:
+            print("You lose.")
